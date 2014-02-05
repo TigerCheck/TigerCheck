858f349455e7700ada227e3447bb7debca7005f7
EthanDarby <ethandarby09@gmail.com>
2014-02-05 12:10:36 -0600

    New Forms
    I added empty form references so others can work on the screens.

---
+++ TigerCheck/AdminScreen.cs
@@ -0,0 +1,103 @@
+﻿//AdminScreen.cs
+//Date Created: 2/5/2014
+//Date Last Modified: 2/5/2014
+//Author of Last Change: Ethan Darby
+//
+//Function: AdminScreen is the first screen users see. When they open the program, this screen will come up and
+// gives them options. They can create a new session, manage sessions, check in or check out patients, and basically do 
+// any admin functions.
+//
+//
+//Important Notes:
+//
+//
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace TigerCheck
+{
+    public partial class AdminScreen : Form
+    {
+        public AdminScreen()
+        {
+            InitializeComponent();
+        }
+
+
+
+/*
+________________________________________________________________
+AdminScreen_Load
+Date Last Modified: 2/5/2014
+Name: Ethan Darby
+
+Functionality: This function is called when the window loads.
+
+Parameters: No info being passed
+
+Returns: Nothing returned
+
+Important notes:
+________________________________________________________________
+*/
+
+
+        private void AdminScreen_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+
+
+/*
+________________________________________________________________
+newSessinButton_Click
+Date Last Modified: 2/5/2014
+Name: Ethan Darby
+
+Functionality: The function is called whenever the user clicks the New Session button (newSessionButton). It loads
+          another screen where the user can select/input data for a new session and create it.
+
+Parameters: It accepts the sender, the button, as it's input. We aren't passing any values.
+
+Returns:    Nothing
+
+Important notes:
+________________________________________________________________
+*/
+
+
+        private void newSessionButton_Click(object sender, EventArgs e)
+        {
+
+            //Open the newSessionScreen
+            Form newSessionForm = new newSessionScreen();
+            newSessionForm.MdiParent = this
+
+
+            
+
+        }
+
+      
+        
+
+      
+    }
+}
---
+++ TigerCheck/CheckOutScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TigerCheck
+{
+    public partial class checkOutScreen : Form
+    {
+        public checkOutScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
