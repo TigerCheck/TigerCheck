48b2c9f2ff2f75149e5b3a742ed6696a5b1dad85
EthanDarby <ethandarby09@gmail.com>
2014-02-05 20:01:22 -0600

    Updates to CheckIn/Out
    I made changes to checkInScreen:
    1) Validated the text boxes for correct data
    2) Check to make sure they all have data
    
    Changes to checkOutScreen:
    1) Forced it to put focus on the text box so that they don't have to
    click and if they use a scanner it will put that value in there. At
    least it should, if there is a text box selected anywhere, an attached
    scanner should just upload a string. We will have to test that
    obviously, but that's how they have worked in my experience.

--- TigerCheck/CheckInScreen.cs
+++ TigerCheck/CheckInScreen.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//CheckInScreen.cs
+//Date Created: 2/5/2014
+//Date Last Modified: 2/5/2014
+//Author of Last Change: Ethan Darby
+//
+//Function: CheckInScreen is called when the Check-In button is clicked from the mains creen. It is a form where the user enters data pertinent to the patient. Thet get general
+//      information and assign them a barcode. When they click submit, the forms are checked and validated before then sending it to the SQL server.
+//
+//Important Notes: The SQL server code is not added here yet, still needs to be done.
+//
+//
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -32,56 +45,119 @@ ________________________________________________________________
 */
         private void submitButton_Click(object sender, EventArgs e)
         {
-            //Declare variables that will be used to capture each value in the boxes
-            String firstName, lastName, race, sex;
-            int age, barcodeNumber;
 
-            //initalize sex variable
-            sex = "None";
-            age = 0;
-            barcodeNumber = 0;
+            //First, lets meke sure that none of them are empty or have invalid values
+            if (allValuesAreValid())
+              {
+                //Declare variables that will be used to capture each value in the boxes
+                String firstName, lastName, race, sex;
+                int age, barcodeNumber;
 
-            //Get the values
-            firstName = firstNameTextBox.Text;
-            lastName = lastNameTextBox.Text;
-            race = raceComboBox.Text;
+                //initalize sex variable
+                sex = "None";
+          
+                //Get the values
+                firstName = firstNameTextBox.Text;
+                lastName = lastNameTextBox.Text;
+                race = raceComboBox.Text;
+                age = Convert.ToInt32(ageComboBox.Text);
+                barcodeNumber = Convert.ToInt32(barcodeNumberTextbox.Text);
+                
+                //Check the radio buttons to see what is selected and then store it
+                if (maleRadioButton.Checked)
+                {
+                    sex = "M";
+                }
 
-            //Catch exceptions here for if the box is empty. Can't covert a string.empty to an int
+                if (femailRadioButton.Checked)
+                {
+                    sex = "F";
+                }
+
+
+
+
+                this.Close();
+
+            } 
+        }
+
+
+/*
+________________________________________________________________
+allValuesAreValid
+Date Last Modified: 2/5/2014
+Name: Ethan Darby
+
+Functionality: This function is called when the submit button is pressed and analyzes all the values in the form.
+
+Parameters: No info being passed
+
+Returns: A bool value is returned indicating True if the values are all valid and False if they are not
+
+Important notes: THIS FUNCTION IS LONGER THAN 30 LINES
+________________________________________________________________
+*/
+        private bool allValuesAreValid()
+        {
+            //local value that will be returned indicating what the function found
+            bool isValid = true;
+
+            //If any of them are blank, then return is not valid
+            if (firstNameTextBox.Text == string.Empty || lastNameTextBox.Text == string.Empty || ageComboBox.Text == string.Empty || barcodeNumberTextbox.Text == string.Empty)
+            {
+                isValid = false;
+                MessageBox.Show("Please enter a value for every required field.");
+            }
+            
+            //make sure that the age is in the required range
             if (ageComboBox.Text != string.Empty)
             {
-                
-                age = Convert.ToInt32(ageComboBox.Text);
+                int valueOfAgeComboBox = Convert.ToInt32(ageComboBox.Text);
+                if (valueOfAgeComboBox > 120 || valueOfAgeComboBox < 1)
+                {
+                    isValid = false;
+                    MessageBox.Show("Age entered was out of range. Age must be greater than 0 and less than 120");
+                    ageComboBox.Focus();
+                }
             }
-            
-            //Catch exception here for if the box is empty. can't covert a string.empty to an int
+
+
+            //Make sure that the barcode entered was actually a barcode, no letters or symbols, and that it is only 4 numbers long
             if (barcodeNumberTextbox.Text != string.Empty)
             {
-                barcodeNumber = Convert.ToInt32(barcodeNumberTextbox.Text);
-            }
-            
+                int numberParseResult = 0;
+                bool isNumber = int.TryParse(barcodeNumberTextbox.Text, out  numberParseResult);
 
-            //Check the radio buttons to see what is selected and then store it
-            if (maleRadioButton.Checked)
-            {
-                sex = "M";
+                //if it's not a number, then it's invalid
+                if (!isNumber)
+                {
+                    isValid = false;
+                    MessageBox.Show("Barcode value is not a number!");
+                    barcodeNumberTextbox.Focus();
+                }
+                if (isNumber)
+                {
+                    if (numberParseResult < 0 || numberParseResult > 9999)
+                    {
+                        isValid = false;
+                        MessageBox.Show("Barcode Value is less than 0 or greater than 4 digits long");
+                        barcodeNumberTextbox.Focus();
+                    }
+
+
+                }
             }
 
-            if(femailRadioButton.Checked)
-            {
-                sex = "F";
-            }
 
-            if (firstName == string.Empty)
+            if (maleRadioButton.Checked == false && femailRadioButton.Checked == false)
             {
-                MessageBox.Show("You must fill in required fields");
-
+                isValid = false;
+                MessageBox.Show("No button selected for sex of the patient");
             }
-            else
-            {
-                MessageBox.Show(firstName + ", " + lastName + ", " + race + " , " + age + ", " + barcodeNumber + ", " + sex);
-                this.Close();
-            }
-          
+            return isValid;
+    
         }
+
     }
 }
--- TigerCheck/CheckOutScreen.cs
+++ TigerCheck/CheckOutScreen.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿//CheckOutScreen.cs
+//Date Created: 2/5/2014
+//Date Last Modified: 2/5/2014
+//Author of Last Change: Ethan Darby
+//
+//Function: This screen is called when the user clicks on the Check-Out button on the admin screen. It allows them to scan or enter a barcode that will then be matched to a student on the
+//      SQL database and they will be checked out.      
+//
+//Important Notes:
+//
+//
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,8 +30,26 @@ namespace TigerCheck
             InitializeComponent();
         }
 
+/*
+________________________________________________________________
+checOutScreen_Load
+Date Last Modified: 2/5/2014
+Name: Ethan Darby
+
+Functionality: This function is called automatically when the checkOutScreen loads.
+
+Parameters: No info being passed
+
+Returns: Nothing returned is returned, but it does hit the sql database and add a patient record
+
+Important notes: This method is used to set things up as they should be for when the screen is loaded. 
+________________________________________________________________
+*/
         private void checkOutScreen_Load(object sender, EventArgs e)
         {
+            //The text box should be focused already so that the scanner can put the value in or they can enter it without having to select it.
+            //This should be defaulted, but just in case we will force it to here. 
+            barcodeTextBox.Focus();
 
         }
     }
