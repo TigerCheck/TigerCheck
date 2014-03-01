a0c181783e11661c0bc2c1c483541854fa1490b8
rossespears <rossespears19@gmail.com>
2014-02-28 23:49:23 -0600

    Heart Rate / Blood Pressure forms
    When I uploaded earlier, I put my files in through the file explorer
    rather than through Visual Studio, Zach got my code transferred, I've
    got my forms fixed and made sure the code got transferred corretly

--- TigerCheck/BloodPressure.Designer.cs
+++ TigerCheck/BloodPressure.Designer.cs
@@ -28,11 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SystolicBloodPressureTextBox = new System.Windows.Forms.TextBox();
+            this.DiatolicBloodPressureTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the patient\'s blood pressure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Systolic";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Diatolic";
+            // 
+            // SystolicBloodPressureTextBox
+            // 
+            this.SystolicBloodPressureTextBox.Location = new System.Drawing.Point(102, 82);
+            this.SystolicBloodPressureTextBox.Name = "SystolicBloodPressureTextBox";
+            this.SystolicBloodPressureTextBox.Size = new System.Drawing.Size(178, 20);
+            this.SystolicBloodPressureTextBox.TabIndex = 3;
+            // 
+            // DiatolicBloodPressureTextBox
+            // 
+            this.DiatolicBloodPressureTextBox.Location = new System.Drawing.Point(102, 112);
+            this.DiatolicBloodPressureTextBox.Name = "DiatolicBloodPressureTextBox";
+            this.DiatolicBloodPressureTextBox.Size = new System.Drawing.Size(178, 20);
+            this.DiatolicBloodPressureTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "mmHG";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "mmHG";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BloodPressure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 232);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DiatolicBloodPressureTextBox);
+            this.Controls.Add(this.SystolicBloodPressureTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "BloodPressure";
             this.Text = "BloodPressure";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox SystolicBloodPressureTextBox;
+        private System.Windows.Forms.TextBox DiatolicBloodPressureTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- TigerCheck/HeartRate.Designer.cs
+++ TigerCheck/HeartRate.Designer.cs
@@ -28,11 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HeartRateTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Patient\'s Heart Rate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Heart Rate";
+            // 
+            // HeartRateTextBox
+            // 
+            this.HeartRateTextBox.Location = new System.Drawing.Point(104, 81);
+            this.HeartRateTextBox.Name = "HeartRateTextBox";
+            this.HeartRateTextBox.Size = new System.Drawing.Size(174, 20);
+            this.HeartRateTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Bpm";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(104, 143);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "OK";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click_1);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(204, 142);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // HeartRate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 183);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HeartRateTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "HeartRate";
             this.Text = "HeartRate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void submitButton_Click_1(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox HeartRateTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
