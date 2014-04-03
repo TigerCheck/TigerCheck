a3747f8f23e2c5cc7916a9d29a05d9d3faea1a56
auzatch <zatchwhite10@yahoo.com>
2014-04-02 22:00:25 -0500

    Android Code
    Added all the Android Code that I had. I wasn't sure what exactly what I
    needed to upload so I did the entire thing.

---
+++ Android/TigerCheck/src/com/example/tigercheck/Welcome.java
@@ -0,0 +1,30 @@
+package com.example.tigercheck;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class Welcome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome, menu);
+		return true;
+	}
+	
+	public void continueButtonPress(View view) {
+		Intent intent = new Intent(this, StationSelect.class);
+		startActivity(intent);
+		finish();
+	}
+
+}
