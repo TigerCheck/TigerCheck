e2909878175e0dec55cbc8e425486f04a29aa420
auzatch <zatchwhite10@yahoo.com>
2014-04-09 17:06:08 -0500

    Added Android Studio code

---
+++ TigerCheckAndroid/app/src/main/java/com/tigercheck/android/app/MainStation.java
@@ -0,0 +1,58 @@
+package com.tigercheck.android.app;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainStation extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_station);
+		
+		Intent intent = getIntent();
+		ArrayList<String> stationList = new ArrayList<String>();
+		stationList = intent.getStringArrayListExtra("key");
+		
+		//Do call barcode scanning activity
+		
+		//Loop through received arraylist and call station activities
+		//int size = stationList.size();
+		//for(int i =0; i < size; i++) {
+			//String check = stationList.get(i);
+			//if(check.equals("R.id.checkbox_blood_Pressure")) {
+				//intent = new Intent(this,BloodPressureStation.class);
+				//startActivity(intent);
+			//}
+			//else if(check.equals("R.id.checkbox_heart_rate")) {
+				//intent = new Intent(this,HeartRateStation.class);
+				//startActivity(intent);
+			//}
+		//}
+		
+		//Call Main station again to go scan a new student
+			//intent = new Intent(this, MainStation.class);
+			//startActivity(intent);
+			//finish();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_station, menu);
+		return true;
+	}
+	
+	public void changeStations (View view) {
+		Intent intent = new Intent(this, StationSelect.class);
+		startActivity(intent);
+		finish();
+	}
+	
+
+}
