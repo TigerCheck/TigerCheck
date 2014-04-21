a50668851dc0bbb544d415af4c66aba102f377be
auzatch <zatchwhite10@yahoo.com>
2014-04-21 13:28:26 -0500

    android changes

--- TigerCheckAndroid/app/src/main/java/com/tigercheck/android/app/MainStation.java
+++ TigerCheckAndroid/app/src/main/java/com/tigercheck/android/app/MainStation.java
@@ -2,11 +2,14 @@ package com.tigercheck.android.app;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.app.AlertDialog;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,7 +19,7 @@ import java.util.ArrayList;
 
 public class MainStation extends Activity {
 
-    static final String SERVER_ADDRESS= "192.168.1.2";
+    static final String SERVER_ADDRESS= "192.168.1.10";
     public int patientBarcode;
     Connection dbCon;
 
@@ -31,12 +34,7 @@ public class MainStation extends Activity {
 		
 		//Do call barcode scanning activity
 
-        try {
-            dbCon.close();
-            Log.w("Connection", "close");
-        } catch (Exception e) {
-            Log.w("Error closing connection", " " + e.getMessage());
-        }
+
         //Call Main station again to go scan a new student
 			//intent = new Intent(this, MainStation.class);
 			//startActivity(intent);
@@ -50,25 +48,67 @@ public class MainStation extends Activity {
 		return true;
 	}
 
-    public void connectToDatabase() {
+    public boolean connectToDatabase() {
+        boolean connectionSucceeded = false;
+
+        AlertDialog alert = new AlertDialog.Builder(this).create();
+        alert.setTitle("Error");
+        alert.setMessage("Unable to connect to database");
+        alert.setButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
-            dbCon = DriverManager.getConnection("jdbc:jtds:sqlserver://192,168.1.2:1433/DATABASE");
+            dbCon = DriverManager.getConnection("jdbc:sqlserver://" + SERVER_ADDRESS + ";");
+            connectionSucceeded = true;
             Log.w("Connection", "open");
         } catch (Exception e) {
+            connectionSucceeded = false;
             Log.w("Error connection", " " + e.getMessage());
+            alert.show();
+        }
+        return connectionSucceeded;
+    }
+
+    public void disconnectFromDatabase() {
+        try {
+            dbCon.close();
+            Log.w("Connection", "close");
+        } catch (Exception e) {
+            Log.w("Error closing connection", " " + e.getMessage());
         }
     }
 
-    private void checkBarcode(int barcode) {
+    public boolean checkBarcode(View view) {
         //check and set
-        patientBarcode = barcode;
-        try {
-            Statement stmt = dbCon.createStatement();
-            Log.w("Barcode check", "statement");
-        } catch (Exception e) {
-            Log.w("Barcode check", " " + e.getMessage());
+        boolean barcodeExists = false;
+        if(connectToDatabase()) {
+            EditText barcodeEntry = (EditText) findViewById(R.id.barcode);
+            int barcode = Integer.parseInt(barcodeEntry.getText().toString());
+
+            AlertDialog alert = new AlertDialog.Builder(this).create();
+            alert.setTitle("Error");
+            alert.setMessage("Barcode not found");
+            alert.setButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            try {
+                Statement stmt = dbCon.createStatement();
+                Log.w("Barcode check", "statement");
+                barcodeExists = true;
+            } catch (Exception e) {
+                Log.w("Barcode check", " " + e.getMessage());
+                alert.show();
+                barcodeExists = false;
+            }
         }
+        return barcodeExists;
     }
 
     private void goThroughStations(ArrayList stations) {
@@ -92,5 +132,9 @@ public class MainStation extends Activity {
 		startActivity(intent);
 		finish();
 	}
+
+    protected void onStop() {
+        disconnectFromDatabase();
+    }
 
 }
